2b9fb52760127568e53332a9454645b791812d62
Touhid Mia <hmtm.cse@gmail.com>
2019-09-10 23:01:20 +0600

    Update things

--- Grails786/grails-app/init/com/hmtmcse/application/BootStrap.groovy
+++ Grails786/grails-app/init/com/hmtmcse/application/BootStrap.groovy
@@ -1,8 +1,15 @@
 package com.hmtmcse.application
 
+import com.hmtmcse.gcommon.ApplicationConfig
+import com.hmtmcse.gcommon.TMGUtil
+import com.hmtmcse.gs.GsConfigHolder
+
 class BootStrap {
 
     def init = { servletContext ->
+        GsConfigHolder.hostnameWithPort = "${TMGUtil.appBaseUrlHostWithPort()}"
+        GsConfigHolder.swaggerDefinitionUrl = "${ApplicationConfig.appBaseUrl()}swaggerUi/definition"
+        println("Bismillah")
     }
     def destroy = {
     }
