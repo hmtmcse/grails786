1ac0c364fd393378b754f337cc8552110a2e0c08
Touhid Mia <hmtm.cse@gmail.com>
2019-11-10 00:30:15 +0600

    User

--- other-plugins/app-api-test/src/main/groovy/com/hmtmcse/test/data/UserData.groovy
+++ other-plugins/app-api-test/src/main/groovy/com/hmtmcse/test/data/UserData.groovy
@@ -6,24 +6,89 @@ class UserData {
             [
                     "firstName": "Touhid",
                     "lastName": "Mia",
-                    "email": "hmtmcse.com@gmail.com",
-                    "password": "123",
-                    "status": "success",
+                    "password": "touhid",
+                    "status": "error",
+                    "message": "Please Enter Email Address.",
             ],
             [
-                    "firstName": "Forida",
-                    "lastName": "Parul",
-                    "email": "forida@gmail.com",
-                    "password": "123",
-                    "status": "success"
+                    "lastName": "Mia",
+                    "email": "touhid@email.local",
+                    "password": "touhid",
+                    "status": "error",
+                    "message": "Please Enter First Name.",
             ],
             [
                     "firstName": "Touhid",
                     "lastName": "Mia",
-                    "email": "hmtmcse.com@gmail.com",
+                    "email": "touhid@email.local",
+                    "password": "touhid",
+                    "status": "success",
+            ],
+            [
+                    "firstName": "Farida",
+                    "lastName": "Parul",
+                    "email": "farida@email.local",
                     "password": "123",
+                    "status": "success"
+            ],
+
+            [
+                    "firstName": "Touhid",
+                    "lastName": "Mia",
+                    "email": "touhid@email.local",
+                    "password": "touhid",
                     "status": "error",
                     "message": "mail already exists.",
+            ],
+
+            [
+                    "firstName": "Mr",
+                    "lastName": "Manager",
+                    "email": "manager@email.local",
+                    "password": "manager",
+                    "status": "success",
+            ],
+            [
+                    "firstName": "Mr",
+                    "lastName": "Marketing",
+                    "email": "marketing@email.local",
+                    "password": "marketing",
+                    "status": "success"
+            ],
+            [
+                    "firstName": "Mr",
+                    "lastName": "Sales",
+                    "email": "sales@email.local",
+                    "password": "sales",
+                    "status": "success"
+            ],
+            [
+                    "firstName": "Mr",
+                    "lastName": "Support",
+                    "email": "support@email.local",
+                    "password": "support",
+                    "status": "success"
+            ],
+            [
+                    "firstName": "System",
+                    "lastName": "Administrator",
+                    "email": "admin@email.local",
+                    "password": "admin",
+                    "status": "success"
+            ],
+            [
+                    "firstName": "Data",
+                    "lastName": "Entry",
+                    "email": "data.entry@email.local",
+                    "password": "data.entry",
+                    "status": "success"
+            ],
+            [
+                    "firstName": "Read",
+                    "lastName": "Only",
+                    "email": "read.only@email.local",
+                    "password": "read.only",
+                    "status": "success"
             ],
     ]
 }
