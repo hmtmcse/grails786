d27a7752c4f694919eb2fb43af7a7ac29134a572
Touhid Mia <touhid@bitmascot.com>
2020-01-02 13:23:27 +0600

    Move Core App to React Material App

--- react-app/scripts/start.sh
+++ react-app/scripts/start.sh
@@ -9,4 +9,7 @@ git clone https://github.com/hmtmcse/tm-react.git
 
 
 echo "Cloning React MUI UI Project From GitHub";
-git clone https://github.com/hmtmcse/react-mui-ui.git 
+git clone https://github.com/hmtmcse/react-mui-ui.git
+
+echo "Cloning React Material Application From GitHub";
+git clone https://github.com/hmtmcse/react-material-app.git
