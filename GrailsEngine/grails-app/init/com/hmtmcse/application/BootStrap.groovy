bd094d762cd534688ea01fb816e25d4265e91f39
Touhid Mia <touhid@bitmascot.com>
2020-01-12 14:15:00 +0600

    Task

--- GrailsEngine/grails-app/init/com/hmtmcse/application/BootStrap.groovy
+++ GrailsEngine/grails-app/init/com/hmtmcse/application/BootStrap.groovy
@@ -2,6 +2,7 @@ package com.hmtmcse.application
 
 import com.hmtmcse.gcommon.ApplicationConfig
 import com.hmtmcse.gcommon.TMGUtil
+import com.hmtmcse.gcommon.ws.AppWebSocketHandler
 import com.hmtmcse.gs.GsConfigHolder
 
 class BootStrap {
@@ -10,6 +11,7 @@ class BootStrap {
         GsConfigHolder.hostnameWithPort = "${TMGUtil.appBaseUrlHostWithPort()}"
         GsConfigHolder.systemVersion = "V2"
         GsConfigHolder.swaggerDefinitionUrl = "${ApplicationConfig.appBaseUrl()}swaggerUi/definition"
+        AppWebSocketHandler.init(servletContext)
         println("Bismillah")
     }
 
